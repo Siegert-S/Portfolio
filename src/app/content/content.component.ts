7e6c5e570e75d2f5a7686da9bde5dda336c9acb8
Sascha Siegert <s.siegert85@googlemail.com>
2025-02-21 13:27:34 +0100

    overhaul of background img

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { AboveTheFoldComponent } from './section/above-the-fold/above-the-fold.component';
-import { AboutComponent } from "./section/about/about.component";
-import { SkillsComponent } from "./section/skills/skills.component";
-import { PortfolioComponent } from "./section/portfolio/portfolio.component";
-import { ContactComponent } from "./section/contact/contact.component";
 import { SectionComponent } from './section/section.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [AboveTheFoldComponent, AboutComponent, SkillsComponent, PortfolioComponent, ContactComponent, SectionComponent],
+  imports: [SectionComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
